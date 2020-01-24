2c9210225fabacb1c7a272e9332aa00d12e81937
MegY <meghdadyazdi@yahoo.com>
2020-01-24 13:10:27 +0000

    initial commit

---
+++ js/sendEmail.js
@@ -0,0 +1,27 @@
+// $(document).ready(function () {
+
+console.log("Hellommmmmmmmmmmm")
+
+// console.log($("#fullname").name)
+// console.log($("#emailaddress").emailaddress)
+// console.log($("#projectsummary").projectsummary)
+//  });
+
+// function sendMail(contactForm) {
+//     // emailjs.send("gmail", "meghdad", {
+//     //     "from_name": contactForm.name.value,
+//     //     "from_email": contactForm.emailaddress.value,
+//     //     "project_request": contactForm.projectsummary.value
+//     // })   
+
+//     // emailjs.send("gmail", "meghdad", {"from_name":"Meghdad","project_request":"I want this","from_email":"meghdadyazdi@yahoo.com"})
+//     .then(
+//         function(response) {
+//             console.log("SUCCESS", response);
+//         },
+//         function(error) {
+//             console.log("FAILED", error);
+//         }
+//     );
+//     return false;  // To block from loading a new page
+// }
