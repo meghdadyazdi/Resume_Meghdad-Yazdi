9197309924bb15eb8bc3d54573d09c1b85dbc73b
MegY <meghdadyazdi@yahoo.com>
2020-01-24 14:41:11 +0000

    fixih emailjs

--- js/sendEmail.js
+++ js/sendEmail.js
@@ -1,27 +1,23 @@
 // $(document).ready(function () {
 
-console.log("Hellommmmmmmmmmmm")
+// console.log("Hellommmmmmmmmmmm")
 
 // console.log($("#fullname").name)
 // console.log($("#emailaddress").emailaddress)
 // console.log($("#projectsummary").projectsummary)
 //  });
 
-// function sendMail(contactForm) {
-//     // emailjs.send("gmail", "meghdad", {
-//     //     "from_name": contactForm.name.value,
-//     //     "from_email": contactForm.emailaddress.value,
-//     //     "project_request": contactForm.projectsummary.value
-//     // })   
-
-//     // emailjs.send("gmail", "meghdad", {"from_name":"Meghdad","project_request":"I want this","from_email":"meghdadyazdi@yahoo.com"})
-//     .then(
-//         function(response) {
-//             console.log("SUCCESS", response);
-//         },
-//         function(error) {
-//             console.log("FAILED", error);
-//         }
-//     );
-//     return false;  // To block from loading a new page
-// }
+function sendMail(contactForm) {
+    emailjs.send("gmail", "meghdad", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    })
+        .then(
+            function (response) {
+                console.log("SUCCESS", response);
+            },
+            function (error) {
+                console.log("FAILED", error);
+            })
+}
